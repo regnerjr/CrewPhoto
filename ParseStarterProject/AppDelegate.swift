9c89aea6aea8e8e962b18f818c8ef289499a4047
John Regner <john@johnregner.com>
2015-07-07 18:33:08 -0500

    Got initial Parse Login View Showing.

--- ParseStarterProject/AppDelegate.swift
+++ ParseStarterProject/AppDelegate.swift
@@ -34,37 +34,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
         PFFacebookUtils.initializeFacebookWithApplicationLaunchOptions(launchOptions)
 
-        PFUser.enableAutomaticUser()
+//        PFUser.enableAutomaticUser()
 
-        let defaultACL = PFACL();
+//        let defaultACL = PFACL();
 
         // If you would like all objects to be private by default, remove this line.
-        defaultACL.setPublicReadAccess(true)
+//        defaultACL.setPublicReadAccess(true)
 
-        PFACL.setDefaultACL(defaultACL, withAccessForCurrentUser:true)
+//        PFACL.setDefaultACL(defaultACL, withAccessForCurrentUser:true)
 
-        if application.applicationState != UIApplicationState.Background {
-            // Track an app open here if we launch with a push, unless
-            // "content_available" was used to trigger a background push (introduced in iOS 7).
-            // In that case, we skip tracking here to avoid double counting the app-open.
-
-            let preBackgroundPush = !application.respondsToSelector("backgroundRefreshStatus")
-            let oldPushHandlerOnly = !self.respondsToSelector("application:didReceiveRemoteNotification:fetchCompletionHandler:")
-            var noPushPayload = false;
-            if let options = launchOptions {
-                noPushPayload = options[UIApplicationLaunchOptionsRemoteNotificationKey] != nil;
-            }
-            if (preBackgroundPush || oldPushHandlerOnly || noPushPayload) {
-                PFAnalytics.trackAppOpenedWithLaunchOptions(launchOptions)
-            }
-        }
-
-//        let types = UIUserNotificationType.Badge |
-//                    UIUserNotificationType.Alert |
-//                    UIUserNotificationType.Sound
-//        application.registerForRemoteNotifications()
-//        application.registerUserNotificationSettings(UIUserNotificationSettings(forTypes: types, categories: nil))
-
+        PFAnalytics.trackAppOpenedWithLaunchOptions(launchOptions)
 
         return FBSDKApplicationDelegate.sharedInstance().application(application, didFinishLaunchingWithOptions: launchOptions)
     }
@@ -73,47 +52,5 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         return FBSDKApplicationDelegate.sharedInstance()
             .application(application, openURL: url, sourceApplication: sourceApplication, annotation: annotation)
     }
-
-    //--------------------------------------
-    // MARK: Push Notifications
-    //--------------------------------------
-
-    func application(application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: NSData) {
-        let installation = PFInstallation.currentInstallation()
-        installation.setDeviceTokenFromData(deviceToken)
-        installation.saveInBackground()
-        
-        PFPush.subscribeToChannelInBackground("") { (succeeded: Bool, error: NSError?) in
-            if succeeded {
-                println("CrewPhotos successfully subscribed to push notifications on the broadcast channel.");
-            } else {
-                println("CrewPhotos failed to subscribe to push notifications on the broadcast channel with error = %@.", error)
-            }
-        }
-    }
-
-    func application(application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: NSError) {
-        if error.code == 3010 {
-            println("Push notifications are not supported in the iOS Simulator.")
-        } else {
-            println("application:didFailToRegisterForRemoteNotificationsWithError: %@", error)
-        }
-    }
-
-    func application(application: UIApplication, didReceiveRemoteNotification userInfo: [NSObject : AnyObject]) {
-        PFPush.handlePush(userInfo)
-        if application.applicationState == UIApplicationState.Inactive {
-            PFAnalytics.trackAppOpenedWithRemoteNotificationPayload(userInfo)
-        }
-    }
-
-    ///////////////////////////////////////////////////////////
-    // Uncomment this method if you want to use Push Notifications with Background App Refresh
-    ///////////////////////////////////////////////////////////
-    // func application(application: UIApplication, didReceiveRemoteNotification userInfo: [NSObject : AnyObject], fetchCompletionHandler completionHandler: (UIBackgroundFetchResult) -> Void) {
-    //     if application.applicationState == UIApplicationState.Inactive {
-    //         PFAnalytics.trackAppOpenedWithRemoteNotificationPayload(userInfo)
-    //     }
-    // }
 
 }
--- ParseStarterProject/ViewController.swift
+++ ParseStarterProject/ViewController.swift
@@ -1,17 +1,35 @@
 import UIKit
 import Parse
-import FBSDKCoreKit
-import FBSDKLoginKit
+import ParseUI
+//import FBSDKCoreKit
+//import FBSDKLoginKit
 
 
 class ViewController: UIViewController {
 
+    let login = PFLogInViewController()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        let loginButton = FBSDKLoginButton()
-        loginButton.center = self.view.center
-        self.view.addSubview(loginButton)
+        login.delegate = self
+        login.fields = PFLogInFields.Facebook
+//        login.logInView?.logo = CrewPhotoView()
+    }
+
+    override func viewDidAppear(animated: Bool) {
+        presentViewController(login, animated: true, completion: nil)
     }
 
 }
 
+extension ViewController: PFLogInViewControllerDelegate {
+    func logInViewController(logInController: PFLogInViewController, didLogInUser user: PFUser) {
+        println("Success Logged in \(user)")
+    }
+    func logInViewControllerDidCancelLogIn(logInController: PFLogInViewController) {
+        print("Do Something on Cancel")
+    }
+    func logInViewController(logInController: PFLogInViewController, didFailToLogInWithError error: NSError?) {
+        print("Login failed with error \(error?.localizedDescription)")
+    }
+}
